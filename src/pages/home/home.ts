60d0747b4bf1cb41b1ac627b5d40d56cdc3f432f
Arek Ruta <arek.ruta89@gmail.com>
2017-07-29 16:22:32 +0200

    takePhoto, saveResults, view image, labels and fab

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { GoogleCloudVisionServiceProvider } from "./../../providers/google-cloud-vision-service/google-cloud-vision-service";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { Component } from "@angular/core";
+import { AlertController } from "ionic-angular";
+import {
+  AngularFireDatabase,
+  FirebaseListObservable
+} from "angularfire2/database";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items: FirebaseListObservable<any[]>;
+  constructor(
+    private camera: Camera,
+    private vision: GoogleCloudVisionServiceProvider,
+    private db: AngularFireDatabase,
+    private alertCtrl: AlertController
+  ) {
+    this.items = db.list("items");
   }
 
+  saveResults(imageData, results) {
+    this.items
+      .push({ imageData: imageData, results: results })
+      .then(_ => {})
+      .catch(err => {
+        this.showAlert(err);
+      });
+  }
+
+  showAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: "Error",
+      subTitle: message,
+      buttons: ["OK"]
+    });
+    alert.present();
+  }
+
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 500,
+      targetWidth: 500,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.vision.getLabels(imageData).subscribe(
+          result => {
+            this.saveResults(imageData, result.json().response);
+          },
+          err => {
+            this.showAlert(err);
+          }
+        );
+      },
+      err => {
+        this.showAlert(err);
+      }
+    );
+  }
 }
